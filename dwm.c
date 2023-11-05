6e0cf1298bdcadfbfeb99067cdc30f94ca08fdc4
zuzlz <zuzlz@mailbox.org>
2023-11-05 12:00:40 +1100

    Added cursor warping. Only when using focusstack or focusmon

--- dwm.c
+++ dwm.c
@@ -932,6 +932,11 @@ focusmon(const Arg *arg)
 	unfocus(selmon->sel, 0);
 	selmon = m;
 	focus(NULL);
+    if (selmon->sel)
+        XWarpPointer(dpy, None, selmon->sel->win, 0, 0, 0, 0, selmon->sel->w/2, selmon->sel->h/2);
+
+    if (!selmon->sel)
+        XWarpPointer(dpy, None, root, 0, 0, 0, 0, selmon->wx + selmon->ww/2, selmon->wy + selmon->wh/2);
 }
 
 void
@@ -957,6 +962,7 @@ focusstack(const Arg *arg)
 	if (c) {
 		focus(c);
 		restack(selmon);
+		XWarpPointer(dpy, None, c->win, 0, 0, 0, 0, c->w/2, c->h/2);
 	}
 }
 
