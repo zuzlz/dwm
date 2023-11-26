26238cb19eb8d77fffeeaeb33b929319857bb347
zuzlz <zuzlz@mailbox.org>
2023-11-26 11:17:55 +1100

    changed some rules

--- config.h
+++ config.h
@@ -12,8 +12,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = {  "SauceCodePro Nerd Font:pixelsize=16:antialias=true:autohint=true"};
-static const char dmenufont[]       = "SauceCodePro Nerd Font:pixelsize=20:antialias=true:autohint=true";
+static const char *fonts[]          = {  "GoMono Nerd Font:pixelsize=16:antialias=true:autohint=true"};
+static const char dmenufont[]       = "GoMono Nerd Font:pixelsize=20:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#A08563";
@@ -33,9 +33,9 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	// { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class           instance    title       tags mask     isfloating   monitor */
+	{ "Steam",         NULL,       NULL,       1 << 8,       0,           0 },
+	{ "steam",         NULL,       NULL,       1 << 8,       0,           0 },
 };
 
 /* layout(s) */
@@ -65,7 +65,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "wezterm", NULL };
 
 static const Key keys[] = {
 	/* modifier                          key                        function             argument */
@@ -87,7 +87,7 @@ static const Key keys[] = {
 	{ MODKEY,                            XK_Tab,                    view,                {0} },
 	{ MODKEY|ShiftMask,                  XK_q,                      killclient,          {0} },
 	{ MODKEY,                            XK_t,                      setlayout,           {.v = &layouts[0]} },
-	{ MODKEY,                            XK_m,                      setlayout,           {.v = &layouts[2]} },
+	{ MODKEY,                            XK_m,                      setlayout,           {.v = &layouts[1]} },
 	{ MODKEY,                            XK_space,                  setlayout,           {0} },
 	{ MODKEY|ShiftMask,                  XK_space,                  togglefloating,      {0} },
 	{ MODKEY,                            XK_0,                      view,                {.ui = ~0 } },
